6b22e3fd8a52085d4ca2f44461410277273743ef
senoiilya <ilia.sienskoi@mail.ru>
2024-06-19 01:49:06 +0300

    Add layout.html, login.html, new-page.html, template.html. Update in main.go: add ServeMux routing, Contorller functions. Add HTTP - Headers in loginHandler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,32 +1,102 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"mmdist/pkg"
 	"net/http"
 )
 
-type msg string
+var types = []string{pkg.PersonalComputerType, pkg.NotebookType, pkg.ServerType, "mono-block"}
 
-func (m msg) ServeHTTP(resp http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(resp, m)
+type ViewData struct {
+	Title   string
+	Message string
 }
 
-var types = []string{pkg.PersonalComputerType, pkg.NotebookType, pkg.ServerType, "monoblock"}
+type ViewData2 struct {
+	Title   string
+	Message []string
+}
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "ui/html/main.html")
-	})
+// статичный файл
+func mainHandler(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "ui/html/layout.html")
+}
 
-	for _, typeName := range types {
-		computer := pkg.New(typeName)
-		if computer == nil {
-			continue
-		}
-		computer.PrintDetails()
+// Controllers
+
+func layoutHandler(w http.ResponseWriter, req *http.Request) {
+	data := ViewData{
+		Title:   "Layout Page",
+		Message: "Здесь находится рут страница",
+	}
+	// если страница не главная, то откроет страницу Page Not Found
+	if req.URL.Path != "/" {
+		http.NotFound(w, req)
+		return
+	}
+	tmpl, _ := template.ParseFiles("ui/html/layout.html")
+	tmpl.Execute(w, data)
+}
+
+func testHandler(w http.ResponseWriter, req *http.Request) {
+	dataArray := ViewData2{
+		Title:   "List of users",
+		Message: []string{"Bob", "Sam", "Tom"},
+	}
+	tmpl, _ := template.ParseFiles("./ui/html/template.html")
+	tmpl.Execute(w, dataArray)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	data := ViewData{
+		Title:   "Login",
+		Message: "Войти",
+	}
+	// устаналвиваем какой HTTP - заголовок нам разрешён
+	w.Header().Set("Allow", http.MethodPost)
+
+	// проверка на метод POST
+	if r.Method != http.MethodPost {
+		w.WriteHeader(405)
+		w.Write([]byte("GET-метод запрещён!"))
+		return
 	}
 
-	fmt.Println("Server is listening...")
-	http.ListenAndServe(":8080", nil)
+	tmpl, _ := template.ParseFiles("./ui/html/login.html")
+	tmpl.Execute(w, data)
+}
+
+func main() {
+	data := ViewData{
+		Title:   "New Page2",
+		Message: "Hello World!",
+	}
+	router := http.NewServeMux()
+	// Использование шаблонов для создания динамических html страниц
+	router.HandleFunc("/", layoutHandler)
+	router.HandleFunc("/test", testHandler)
+	router.HandleFunc("/new", func(w http.ResponseWriter, req *http.Request) {
+		tmpl, _ := template.ParseFiles("./ui/html/new-page.html")
+		tmpl.Execute(w, data)
+	})
+	router.HandleFunc("/edit", func(w http.ResponseWriter, req *http.Request) {
+		tmpl, _ := template.ParseFiles("./ui/html/template.html")
+		tmpl.Execute(w, data)
+	})
+	router.HandleFunc("/login", loginHandler)
+
+	// Вывод классов
+	//for _, typeName := range types {
+	//	computer := pkg.New(typeName)
+	//	if computer == nil {
+	//		continue
+	//	}
+	//	computer.PrintDetails()
+	//}
+
+	log.Println("Сервер запущен на localhost:8080")
+	err := http.ListenAndServe(":4000", router)
+	log.Fatal(err)
 }
