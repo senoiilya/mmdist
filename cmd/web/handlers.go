3e8d8d5840b5db36fe021fc26e52ac01caa7ec5e
senoiilya <ilia.sienskoi@mail.ru>
2024-06-19 18:25:45 +0300

    add Controllers in handlers.go, changes layout.html and main.go

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,1 +1,119 @@
 package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+// Controllers
+
+func home(w http.ResponseWriter, req *http.Request) {
+	data := ViewData{
+		Title:   "Layout Page",
+		Message: "Здесь находится рут страница",
+	}
+
+	files := []string{"./ui/html/layout.html", "./ui/html/viewLayout.html"}
+
+	// если страница не главная, то откроет страницу Page Not Found
+	if req.URL.Path != "/" {
+		http.NotFound(w, req)
+		return
+	}
+
+	//tmpl, err := template.ParseFiles("./ui/html/layout.html")
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+}
+
+func test(w http.ResponseWriter, req *http.Request) {
+	dataArray := ViewData2{
+		Title:   "List of users",
+		Message: []string{"Bob", "Sam", "Tom"},
+	}
+	tmpl, err := template.ParseFiles("./ui/html/template.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = tmpl.Execute(w, dataArray)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	data := ViewData{
+		Title:   "Login",
+		Message: "Войти",
+	}
+	// устаналвиваем какой HTTP - заголовок нам разрешён
+	w.Header().Set("Allow", http.MethodPost)
+
+	// упраление картой заголовков напрямую
+	//w.Header()["Date"] = nil
+
+	// проверка на метод POST
+	if r.Method != http.MethodPost {
+		// Не продвинутый подход к отправке ответа
+		//w.WriteHeader(405) // Записываем заголовок
+		//w.Write([]byte("GET-метод запрещён!"))
+		//return
+		http.Error(w, "Метод запрещён!", 405)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("./ui/html/login.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func newTest(w http.ResponseWriter, req *http.Request) {
+	// Извлекаем значение параметра id из URL и попытаемся
+	// конвертировать строку в integer используя функцию strconv.Atoi(). Если его нельзя
+	// конвертировать в integer, или значение меньше 1, возвращаем ответ
+	// 404 - страница не найдена!
+	id, err := strconv.Atoi(req.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, req)
+		return
+	}
+	data := ViewData3{
+		Title:   "New Page2",
+		Message: "Hello World!",
+		Id:      id,
+	}
+	tmpl, err := template.ParseFiles("./ui/html/new-page.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"html/template"
 	"log"
 	"mmdist/pkg"
 	"net/http"
-	"strconv"
 )
 
 var types = []string{pkg.PersonalComputerType, pkg.NotebookType, pkg.ServerType, "mono-block"}
@@ -28,95 +26,27 @@ type ViewData3 struct {
 }
 
 // статичный файл
-func mainHandler(w http.ResponseWriter, req *http.Request) {
-	http.ServeFile(w, req, "ui/html/layout.html")
-}
-
-// Controllers
-
-func layoutHandler(w http.ResponseWriter, req *http.Request) {
-	data := ViewData{
-		Title:   "Layout Page",
-		Message: "Здесь находится рут страница",
-	}
-	// если страница не главная, то откроет страницу Page Not Found
-	if req.URL.Path != "/" {
-		http.NotFound(w, req)
-		return
-	}
-	tmpl, _ := template.ParseFiles("ui/html/layout.html")
-	tmpl.Execute(w, data)
-}
-
-func testHandler(w http.ResponseWriter, req *http.Request) {
-	dataArray := ViewData2{
-		Title:   "List of users",
-		Message: []string{"Bob", "Sam", "Tom"},
-	}
-	tmpl, _ := template.ParseFiles("./ui/html/template.html")
-	tmpl.Execute(w, dataArray)
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	data := ViewData{
-		Title:   "Login",
-		Message: "Войти",
-	}
-	// устаналвиваем какой HTTP - заголовок нам разрешён
-	w.Header().Set("Allow", http.MethodPost)
-
-	// упраление картой заголовков напрямую
-	//w.Header()["Date"] = nil
-
-	// проверка на метод POST
-	if r.Method != http.MethodPost {
-		// Не продвинутый подход к отправке ответа
-		//w.WriteHeader(405) // Записываем заголовок
-		//w.Write([]byte("GET-метод запрещён!"))
-		//return
-		http.Error(w, "Метод запрещён!", 405)
-		return
-	}
-
-	tmpl, _ := template.ParseFiles("./ui/html/login.html")
-	tmpl.Execute(w, data)
-}
-
-func newTestHandler(w http.ResponseWriter, req *http.Request) {
-	// Извлекаем значение параметра id из URL и попытаемся
-	// конвертировать строку в integer используя функцию strconv.Atoi(). Если его нельзя
-	// конвертировать в integer, или значение меньше 1, возвращаем ответ
-	// 404 - страница не найдена!
-	id, err := strconv.Atoi(req.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, req)
-		return
-	}
-	data := ViewData3{
-		Title:   "New Page2",
-		Message: "Hello World!",
-		Id:      id,
-	}
-	tmpl, _ := template.ParseFiles("./ui/html/new-page.html")
-	tmpl.Execute(w, data)
-}
+//func mainHandler(w http.ResponseWriter, req *http.Request) {
+//	http.ServeFile(w, req, "ui/html/layout.html")
+//}
 
 func main() {
-	data := ViewData{
-		Title:   "New Page2",
-		Message: "Hello World!",
-	}
-	//router := http.NewServeMux()
+	//data := ViewData{
+	//	Title:   "New Page2",
+	//	Message: "Hello World!",
+	//}
 	router := mux.NewRouter()
+
 	// Использование шаблонов для создания динамических html страниц
-	router.HandleFunc("/", layoutHandler)
-	router.HandleFunc("/test", testHandler)
-	router.HandleFunc("/new", newTestHandler)
-	router.HandleFunc("/edit", func(w http.ResponseWriter, req *http.Request) {
-		tmpl, _ := template.ParseFiles("./ui/html/template.html")
-		tmpl.Execute(w, data)
-	})
-	router.HandleFunc("/login", loginHandler)
+	router.HandleFunc("/", home)
+
+	router.HandleFunc("/test", test)
+	router.HandleFunc("/new", newTest)
+	//router.HandleFunc("/edit", func(w http.ResponseWriter, req *http.Request) {
+	//	tmpl, _ := template.ParseFiles("./ui/html/template.html")
+	//	tmpl.Execute(w, data)
+	//})
+	router.HandleFunc("/login", login)
 
 	// Вывод классов
 	//for _, typeName := range types {
@@ -127,7 +57,7 @@ func main() {
 	//	computer.PrintDetails()
 	//}
 
-	log.Println("Сервер запущен на localhost:8080")
+	log.Println("Сервер запущен на localhost:4000")
 	err := http.ListenAndServe(":4000", router)
 	log.Fatal(err)
 }
