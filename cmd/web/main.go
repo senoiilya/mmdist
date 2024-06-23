fd974c627e67f2da0a756687d061ae88bfe7e50b
senoiilya <ilia.sienskoi@mail.ru>
2024-06-23 21:38:59 +0300

    delete necessary logic in main.go and insert it in routes.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,15 +6,14 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"time"
 )
 
 //var types = []string{pkg.PersonalComputerType, pkg.NotebookType, pkg.ServerType, "mono-block"}
 
+// Структура для флагов
 type FlagsConfig struct {
-	Addr      string
-	StaticDir string
+	Addr string
 }
 
 // Используем патерн Dependency Injection
@@ -43,34 +42,6 @@ type ViewLayout struct {
 	Title    string
 	Message  string
 	Computer string
-}
-
-// Настраиваемая файловая система, не позволяет пользователю открывать папки в static на сайте
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, _ := f.Stat()
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-
-			return nil, err
-		}
-	}
-
-	return f, nil
 }
 
 func main() {
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,7 +3,36 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"net/http"
+	"path/filepath"
 )
+
+// Настраиваемая файловая система, не позволяет пользователю открывать папки в static на сайте
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, _ := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
+}
 
 func (app *application) routes() *mux.Router {
 	// маршрутизатор HTTP запросов
