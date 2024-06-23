eb1a125993f33876848db4fee90071c23faff778
senoiilya <ilia.sienskoi@mail.ru>
2024-06-23 15:43:35 +0300

    delete in layout.html necessary string and add neutered file system

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/senoiilya/mmdist/pkg"
 	"log"
 	"net/http"
+	"path/filepath"
 	"time"
 )
 
@@ -56,7 +57,7 @@ func main() {
 	//	computer.PrintDetails()
 	//}
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fileServer))
 
 	srv := &http.Server{
@@ -69,4 +70,32 @@ func main() {
 	log.Println("Сервер запущен на localhost:4000")
 	err := srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+// Настраиваемая файловая система, не позволяет пользователю открывать папки в static на сайте
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
