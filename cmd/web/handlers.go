67ac4ebf95435bccf2f90f112a7c19ecc967d26a
senoiilya <ilia.sienskoi@mail.ru>
2024-06-24 02:48:23 +0300

    fixes in handlers.go

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,10 +13,12 @@ func (app *application) home(w http.ResponseWriter, req *http.Request) {
 	data := ViewLayout{
 		Title:    "Домашняя страница",
 		Message:  "Здесь находится рут страница",
+		IsAuth:   true,
+		Username: "Илья",
 		Computer: pkg.New(pkg.NotebookType).String(),
 	}
 
-	files := []string{"./ui/html/layout.html", "./ui/html/viewLayout.html"}
+	files := []string{"./ui/html/layout.html", "./ui/html/viewLayout.html", "./ui/html/loginpartial.html"}
 
 	// если страница не главная, то откроет страницу Page Not Found
 	if req.URL.Path != "/" {
@@ -38,7 +40,8 @@ func (app *application) home(w http.ResponseWriter, req *http.Request) {
 
 func (app *application) login(w http.ResponseWriter, req *http.Request) {
 	dataArray := ViewData2{
-		Title:   "List of users",
+		Title:   "Войти",
+		IsAuth:  false,
 		Message: []string{"Bob", "Sam", "Tom"},
 	}
 	/// тут чекнуть
@@ -48,7 +51,7 @@ func (app *application) login(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	files := []string{"./ui/html/layout.html", "./ui/html/login.html"}
+	files := []string{"./ui/html/layout.html", "./ui/html/login.html", "./ui/html/loginpartial.html"}
 
 	tmpl, err := template.ParseFiles(files...)
 	if err != nil {
@@ -65,21 +68,22 @@ func (app *application) login(w http.ResponseWriter, req *http.Request) {
 
 func (app *application) registration(w http.ResponseWriter, r *http.Request) {
 	data := ViewData{
-		Title:   "Login",
-		Message: "Войти",
+		Title:   "Регистрация",
+		Message: "Пожалуйста, заполните форму регистрации",
+		IsAuth:  false,
 	}
-	files := []string{"./ui/html/layout.html", "../ui/html/login.html"}
+	files := []string{"./ui/html/layout.html", "./ui/html/registration.html", "./ui/html/loginpartial.html"}
 
 	// упраление картой заголовков напрямую
 	//w.Header()["Date"] = nil
 
 	// проверка на метод POST
-	if r.Method != http.MethodPost {
-		// устаналвиваем какой HTTP - заголовок нам разрешён
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	//if r.Method != http.MethodPost {
+	//	// устаналвиваем какой HTTP - заголовок нам разрешён
+	//	w.Header().Set("Allow", http.MethodPost)
+	//	app.clientError(w, http.StatusMethodNotAllowed)
+	//	return
+	//}
 
 	tmpl, err := template.ParseFiles(files...)
 	if err != nil {
@@ -99,16 +103,18 @@ func (app *application) userPage(w http.ResponseWriter, req *http.Request) {
 	// конвертировать в integer, или значение меньше 1, возвращаем ответ
 	// 404 - страница не найдена!
 	id, err := strconv.Atoi(req.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		app.notFound(w)
-		return
-	}
+	//if err != nil || id < 1 {
+	//	app.notFound(w)
+	//	return
+	//}
+	// позже вернуть
 	data := ViewData3{
 		Title:   "New Page2",
 		Message: "Hello World!",
 		Id:      id,
+		IsAuth:  false,
 	}
-	files := []string{"./ui/html/layout.html", "./ui/html/registration.html"}
+	files := []string{"./ui/html/layout.html", "./ui/html/userpage.html", "./ui/html/loginpartial.html"}
 
 	tmpl, err := template.ParseFiles(files...)
 	if err != nil {
@@ -124,7 +130,7 @@ func (app *application) userPage(w http.ResponseWriter, req *http.Request) {
 }
 
 func (app *application) products(w http.ResponseWriter, req *http.Request) {
-	files := []string{"./ui/html/layout.html", "./ui/html/products.html"}
+	files := []string{"./ui/html/layout.html", "./ui/html/products.html", "./ui/html/loginpartial.html"}
 	data := ViewLayout{Title: "Products", Message: "fsdfsd"}
 
 	tmpl, err := template.ParseFiles(files...)
@@ -137,5 +143,19 @@ func (app *application) products(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		app.serverError(w, err)
 		return
+	}
+}
+
+func (app *application) cart(w http.ResponseWriter, r *http.Request) {
+	files := []string{"./ui/html/layout.html", "./ui/html/cart.html", "./ui/html/loginpartial.html"}
+
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	err = tmpl.ExecuteTemplate(w, "layout", nil)
+	if err != nil {
+		app.serverError(w, err)
 	}
 }
